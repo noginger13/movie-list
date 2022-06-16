35019aab2f333a1ad4bdeec6578250b6b6a2c099
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-16 15:55:35 -0400

    added initial stub out of api call

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,61 +1,72 @@
 import React from 'react';
 import MovieAdd from './MovieAdd.jsx';
-import MovieListSearch from './MovieListSearch.jsx';
+import MovieListNav from './MovieListNav.jsx';
 import MovieList from './MovieList.jsx';
+import movieDBSearch from '../movieDBSearch.js';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: [
-        {title: 'Mean Girls'},
-        {title: 'Hackers'},
-        {title: 'The Grey'},
-        {title: 'Sunshine'},
-        {title: 'Ex Machina'},
+      toWatch: [
+        {title: 'Add a movie!'}
       ],
-      search: [{title: ''}]
+      watched:[ {title: 'Add a movie!'}],
+      search: [ {title: 'Look for a movie!'}]
     };
     this.handleSubmitAdd = this.handleSubmitAdd.bind(this);
     this.searchMovies = this.searchMovies.bind(this);
 
   }
 
-  //TODO: Add functions to search, addmovies, click on movies
+  //TODO: Set initial state to popular movies
+  componentDidMount(){
+    // movieDBSearch();
+  }
 
   handleSubmitAdd(movie) {
     event.preventDefault();
-    var newMovies = this.state.movies.slice();
+    var newMovies = this.state.toWatch.slice();
     newMovies.push({title: movie});
-    this.setState(this.state.movies = newMovies);
+    this.setState(this.state.toWatch = newMovies);
+  }
+
+  handleWatchedClick(event) {
+    event.preventDefault();
+    this.setState()
   }
 
   searchMovies(query) {
-    var searchMovies = this.state.movies.slice();
+    var searchMovies = this.state.toWatch.slice();
     var results = [];
+    this.setState({view: 'search'});
     for (var movie of searchMovies) {
       if (movie.title.indexOf(query) !== -1) {
         results.push(movie);
       }
     }
     console.log(results);
-    this.setState({search: results});
+    this.setState({view: 'search', search: results});
   }
 
   render () {
     return (
       <div>
         <h1>Movie List</h1>
-        <MovieAdd movieAdd={this.handleSubmitAdd}/>
-        <MovieListSearch movieSearch={this.searchMovies}/>
-        <MovieList movies={this.state.movies}/>
+        <MovieAdd movieAdd={this.handleSubmitAdd.bind(this)}/>
+        <MovieListNav movieSearch={this.searchMovies.bind(this)}/>
+        <MovieList
+          view={this.state.view}
+          watched={this.state.watched}
+          toWatch={this.state.toWatch}
+          search={this.state.search}
+        />
       </div>
     )
   }
 
 }
-
 
 
 export default App;
