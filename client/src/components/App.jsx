65971c7701746b0eebcbb625e69e623cf8e1e696
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-15 20:12:32 -0400

    add search functionality to page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
         {title: 'Sunshine'},
         {title: 'Ex Machina'},
       ],
-      results: [{title: ''}]
+      search: [{title: ''}]
     };
     this.handleSubmitAdd = this.handleSubmitAdd.bind(this);
     this.searchMovies = this.searchMovies.bind(this);
@@ -24,28 +24,23 @@ class App extends React.Component {
 
   //TODO: Add functions to search, addmovies, click on movies
 
-  handleSubmitAdd(event) {
+  handleSubmitAdd(movie) {
     event.preventDefault();
     var newMovies = this.state.movies.slice();
-    newMovies.push({title: event.target.value});
+    newMovies.push({title: movie});
     this.setState(this.state.movies = newMovies);
   }
 
-  handleClick(event) {
-    event.preventdefault();
-    console.log()
-  }
-
-  searchMovies(event) {
-    var query = event.target.value;
+  searchMovies(query) {
     var searchMovies = this.state.movies.slice();
     var results = [];
-    for (movie of searchMovies) {
-      if (movie.title.indexOf(query) > 0) {
+    for (var movie of searchMovies) {
+      if (movie.title.indexOf(query) !== -1) {
         results.push(movie);
       }
     }
-    this.setState(this.state.results = results);
+    console.log(results);
+    this.setState({search: results});
   }
 
   render () {
@@ -53,7 +48,7 @@ class App extends React.Component {
       <div>
         <h1>Movie List</h1>
         <MovieAdd movieAdd={this.handleSubmitAdd}/>
-        <MovieListSearch movies={this.state.movies}/>
+        <MovieListSearch movieSearch={this.searchMovies}/>
         <MovieList movies={this.state.movies}/>
       </div>
     )
